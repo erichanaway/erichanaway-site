66efe6075c9b674ee063bccd2bc379d5f3147169
Eric Hanaway <erichanaway@yahoo.com>
2026-06-01 20:32:21 -0700

    Configure Vite multi-page build

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        project1: resolve(__dirname, 'project1.html'),
+      },
+    },
+  },
+})
