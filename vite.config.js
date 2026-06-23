f010d6ea09a2e49ad5fef4191d630d4001e4cf63
Eric Hanaway <erichanaway@yahoo.com>
2026-06-22 18:42:39 -0700

    Add Resume and Contact pages to new Vite build.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         projects: resolve(__dirname, 'projects.html'),
         project1: resolve(__dirname, 'project1.html'),
+        project1: resolve(__dirname, 'resume.html'),
+        project1: resolve(__dirname, 'contact.html'),
+
       },
     },
   },
